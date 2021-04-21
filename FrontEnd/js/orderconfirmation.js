5954125e166d669b15041c2df1ba9f48759d533c
NicoJob13 <nico.jobard@yahoo.com>
2021-04-21 16:34:32 +0200

    Ajout d'une instruction pour vider le localStorage.

--- FrontEnd/js/orderconfirmation.js
+++ FrontEnd/js/orderconfirmation.js
@@ -5,3 +5,6 @@ orderPrice.textContent = JSON.parse(localStorage.getItem('orderPrice'));
 //Création d'une constante ciblant l'élement d'id "orderId" pour y injecter l'id de la commande précédemment stocké dans le localStorage
 const orderId = document.getElementById('orderId');
 orderId.textContent = JSON.parse(localStorage.getItem('orderId'));
+
+//Vidage du localStorage
+localStorage.clear();
