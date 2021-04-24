9b902f6ebf62de7c549abbe77198bbbd77a3f8f5
NicoJob13 <nico.jobard@yahoo.com>
2021-04-24 16:56:18 +0200

    Page d'accueil : redécoupage des fonctions JS en des fonctions plus courtes pour une meilleure lecture.

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,41 +1,35 @@
 //Le fichier index.js est destiné à gérer les actions sur la page d'accueil du site
 
-//On appelle la fonction permettant l'affichage de la galerie de produits
-displayProducts();
+//Appel de la fonction permettant l'affichage de la galerie de produits
+displayProductsGallery();
 
-async function displayProducts() {/*Cette fonction est asynchrone pour au préalable récupérer les
-    informations sur les produits depuis le serveur via la fonction 'getProducts' passée en constante et
-    dont on attend la réponse*/
+async function displayProductsGallery() {/*Fonction asynchrone car il faut au préalable récupérer les
+    informations sur les produits depuis le serveur via la fonction 'getProducts'*/
     const products = await getProducts();
 
-    for (product of products) {/*Avec une boucle, pour chaque produit on crée une card par appel de la fonction
-        'createProductsGallery*/
-        createProductsGallery(product);
+    createProductsGallery(); //Puis appel de la fonction créant la galerie
+
+    for (product of products) {/*Pour chaque produit, création d'une card et intégration à la galerie
+    par appel de la fonction 'createProductCard'*/
+        createProductCard(product);
     }
 }
 
-//Fonction permettant d'interroger le serveur via une requête GET réalisée à l'aide de l'API fetch
-function getProducts() {//Cette fonction va retourner le résultat de la requête
-    return fetch(`http://localhost:3000/api/teddies`) //Adresse du serveur passée en paramètre de la requête
-        .then(function(response) {//On récupère la réponse sous forme d'objet JSON
-        return response.json();
-        })
-        .then(function(products) {//Puis on récupère les données
-        return products;
-        })
-        .catch(function(error) {//En cas d'erreur elle est affichée dans la console
-        console.log(error);
-    });
+//Fonction créant la galerie que l'on intègre dans la balise HTML d'id "productSection"
+function createProductsGallery() {
+    const productsSection = document.getElementById('productSection'); //On cible l'élément d'id 'productSection'
+    const productsGallery = document.createElement('div'); //On crée une div...
+    productsGallery.id = 'productsGallery';
+    productsGallery.classList.add('gallery');
+    productsSection.appendChild(productsGallery); //...que l'on intègre dans l'élément d'id 'productSection'
 }
 
-//Fonction permettant de créer la galerie, les cards des produits et de les y intégrer
-function createProductsGallery() {
-    const productsSection = document.getElementById('productSection'); //On cible l'élément d'id 'productSection
-    const productsGallery = document.createElement('div'); //On crée une div...
-    productsSection.appendChild(productsGallery); //...que l'on intègre dans l'élément précédent
+//Fonction créant une card de produit et l'intégrant à la galerie
+function createProductCard() {
+    // On cible l'élément d'id 'productsGallery'
+    const productsGallery = document.getElementById('productsGallery');
 
-    /*On crée successivement les éléments nécessaires à la constitution d'une card, auxquels on ajoute les classes CSS souhaitées
-    (.classList.add) et le contenu en faisant aux propriétés disponibles pour l'objet récupéré du serveur et/ou la balise utilisée*/
+    /*Création des éléments nécessaires pour une card, ajout des classes CSS et du contenu*/
     //La card
     const galleryCard = document.createElement('article');
     galleryCard.classList.add('card', 'galleryCard');
@@ -65,7 +59,7 @@ function createProductsGallery() {
     productPrice.classList.add('productPrice');
     productPrice.textContent = product.price / 100 + ",00 €";
     
-    /*On intègre les éléments les uns dans les autres afin de créer la card, elle-même intégrée à la galerie*/
+    /*On intègre les éléments les uns dans les autres afin de créer la card et de l'intégrer à la galerie*/
     productsGallery.appendChild(galleryCard);
     galleryCard.appendChild(productLink);
     productLink.appendChild(pictureContainer);
@@ -73,4 +67,18 @@ function createProductsGallery() {
     productLink.appendChild(galleryCardText);
     galleryCardText.appendChild(productCardTitle);
     galleryCardText.appendChild(productPrice);
+}
+
+//Fonction permettant d'interroger le serveur via une requête GET réalisée à l'aide de l'API fetch
+function getProducts() {//Cette fonction va retourner le résultat de la requête
+    return fetch(`http://localhost:3000/api/teddies`) //Adresse du serveur passée en paramètre de la requête
+        .then(function(response) {//On récupère la réponse sous forme d'objet JSON
+        return response.json();
+        })
+        .then(function(products) {//Puis on récupère les données
+        return products;
+        })
+        .catch(function(error) {//En cas d'erreur elle est affichée dans la console
+        console.log(error);
+    });
 }
